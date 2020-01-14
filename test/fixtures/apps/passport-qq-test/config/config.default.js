a003d99300d1e603161b9f06c00f427e046d7bf3
spencezhang <spencezhang@tencent.com>
2020-01-14 23:31:45 +0800

    ci: fix env

--- test/fixtures/apps/passport-qq-test/config/config.default.js
+++ test/fixtures/apps/passport-qq-test/config/config.default.js
@@ -3,6 +3,6 @@
 exports.keys = 'foo';
 
 exports.passportQQ = {
-  key: process.env.EGG_PASSPORT_QQ_CLIENT_ID,
-  secret: process.env.EGG_PASSPORT_QQ_CLIENT_SECRET,
+  key: '123', // process.env.EGG_PASSPORT_QQ_CLIENT_ID,
+  secret: '123', // process.env.EGG_PASSPORT_QQ_CLIENT_SECRET,
 };
